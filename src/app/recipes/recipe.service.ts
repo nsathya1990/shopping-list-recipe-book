d5fc3f64dba39bfed3a825db26ad05fc30f23ebe
Sathyapriyan Nadar -X (sanadar - TECH MAHINDRA LIM at Cisco) <sanadar@partnet.cisco.com>
2020-04-18 17:52:51 +0530

    Managing recipe in recipe service

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,20 @@
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+
+    private recipes: Recipe[] = [
+        new Recipe('Meat', 'Meat is animal flesh that is eaten as food',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+        new Recipe('Hot Dog', 'Meat is animal flesh that is eaten as food',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+    ];
+
+    /**
+     * The slice() method will return a new array which is an exact copy of this array.
+     * Therefore, we don't get a reference of this array; we just get a copy.
+     */
+    getRecipes() {
+        return this.recipes.slice();
+    }
+
+}
