8c5c467c595a1780442638de6576026ac68c362c
Sathyapriyan Nadar -X (sanadar - TECH MAHINDRA LIM at Cisco) <sanadar@partnet.cisco.com>
2020-04-25 17:09:06 +0530

    passing dynamic parameters to links

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,7 +9,8 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent implements OnInit {
 
- @Input() recipe: Recipe;
+  @Input() index: number;
+  @Input() recipe: Recipe;
 
   ngOnInit(): void {
   }
