7bc0d214222c6582fdb11e66d304706b2cd924c7
Sathyapriyan Nadar -X (sanadar - TECH MAHINDRA LIM at Cisco) <sanadar@partnet.cisco.com>
2020-04-25 19:41:28 +0530

    retrieving route parameters

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeEditComponent implements OnInit {
 
-  constructor() { }
+  editMode = false;
+  id: number;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params: Params) => {
+      this.id = +params.id;
+      this.editMode = params.id != null;
+      console.log(this.editMode);
+    });
   }
 
 }
