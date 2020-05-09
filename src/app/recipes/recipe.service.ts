a9cb4e1ecf53d4687bf11b96752d741658019ec0
Sathyapriyan Nadar -X (sanadar - TECH MAHINDRA LIM at Cisco) <sanadar@partnet.cisco.com>
2020-05-09 19:58:47 +0530

    redirecting the user after deleting a recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -58,7 +58,7 @@ export class RecipeService {
     }
 
     deleteRecipe(index: number): void {
-        this.recipes.slice(index, 1);
+        this.recipes.splice(index, 1);
         this.recipesChanged.next(this.recipes.slice());
     }
 
