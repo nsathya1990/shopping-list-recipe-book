3b03040756668305068e19a57a8ebf98f60db9cb
Sathyapriyan Chinnathangam <nsathya1990@gmail.com>
2020-05-14 06:41:35 +0530

    storing recipes

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { RecipeService } from '../recipes/recipe.service';
+
 // Since we are injecting the HTTPClient service into this service, we add the @Injectable() decorator
 @Injectable({
     providedIn: 'root'
 })
 export class DataStorageService {
 
-    constructor(private httpClient: HttpClient) { }
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService) { }
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.httpClient.put('https://ng-shooping-list-recipe-book.firebaseio.com/recipes.json', recipes)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
 
 }
