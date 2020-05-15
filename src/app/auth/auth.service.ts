70c1a46abe74c921dbac6fbf842d9722c770578c
Sathyapriyan Chinnathangam <nsathya1990@gmail.com>
2020-05-15 19:58:08 +0530

    sending login requests

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,12 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: boolean;
 }
 
 @Injectable({
@@ -47,6 +48,15 @@ export class AuthService {
                 }
                 return throwError(errorMessage);
             }));
+    }
+
+    login(email: string, password: string): Observable<AuthResponseData> {
+        return this.httpClient
+        .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.API_KEY, {
+            email,
+            password,
+            returnSecureToken: true
+        });
     }
 
 }
