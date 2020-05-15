dd056f7b575e10ea0463f95adfac494410420fe8
Sathyapriyan Chinnathangam <nsathya1990@gmail.com>
2020-05-16 03:50:16 +0530

    adding auto-login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -56,6 +56,22 @@ export class AuthService {
                 }));
     }
 
+    autoLogin(): void {
+        const userData: {
+            email: string,
+            id: string,
+            _token: string,
+            _tokenExpirationDate: string
+        } = JSON.parse(localStorage.getItem('userData')); // synchronous method
+        if (!userData) {
+            return;
+        }
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+        if (loadedUser.token) {
+            this.user.next(loadedUser);
+        }
+    }
+
     logout(): void {
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -66,6 +82,7 @@ export class AuthService {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
         this.user.next(user);
+        localStorage.setItem('userData', JSON.stringify(user));
     }
 
     private handleError(errorRes: HttpErrorResponse): Observable<string> {
