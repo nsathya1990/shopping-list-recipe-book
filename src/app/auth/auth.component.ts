6334445af5d6785a362f7d23692181c417fccd25
Sathyapriyan Chinnathangam <nsathya1990@gmail.com>
2020-05-15 21:24:21 +0530

    login error handling

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -26,7 +26,7 @@ export class AuthComponent {
         }
         const email = form.value.email;
         const password = form.value.password;
-        let authObs: Observable<AuthResponseData>;
+        let authObs: Observable<AuthResponseData | string>;
         this.isLoading = true;
         if (this.isLoginMode) {
             authObs = this.authService.login(email, password);
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
@@ -21,42 +21,50 @@ export class AuthService {
 
     constructor(private httpClient: HttpClient) { }
 
-    signup(email: string, password: string): Observable<AuthResponseData> {
+    signup(email: string, password: string): Observable<AuthResponseData | string> {
         return this.httpClient
             .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + this.API_KEY, {
                 email,
                 password,
                 returnSecureToken: true
-            }).pipe(catchError(errorRes => {
-                let errorMessage = 'An unknown error occurred';
-                if (!errorRes.error || !errorRes.error.error) {
-                    return throwError(errorMessage);
-                }
-                // this scenario will fail for some error such as network failure, therefore we added the condition above
-                switch (errorRes.error.error.message) {
-                    case 'EMAIL_EXISTS':
-                        errorMessage = 'The email address is already in use by another account.';
-                        break;
-                    case 'OPERATION_NOT_ALLOWED':
-                        errorMessage = 'Password sign-in is disabled for this project';
-                        break;
-                    case 'TOO_MANY_ATTEMPTS_TRY_LATER':
-                        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
-                        break;
-                    default:
-                        break;
-                }
-                return throwError(errorMessage);
-            }));
+            }).pipe(catchError(this.handleError));
     }
 
-    login(email: string, password: string): Observable<AuthResponseData> {
+    login(email: string, password: string): Observable<AuthResponseData | string> {
         return this.httpClient
-        .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.API_KEY, {
-            email,
-            password,
-            returnSecureToken: true
-        });
+            .post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + this.API_KEY, {
+                email,
+                password,
+                returnSecureToken: true
+            }).pipe(catchError(this.handleError));
+    }
+
+    private handleError(errorRes: HttpErrorResponse): Observable<string> {
+        let errorMessage = 'An unknown error occurred';
+        if (!errorRes.error || !errorRes.error.error) {
+            return throwError(errorMessage);
+        }
+        // this scenario will fail for some error such as network failure, therefore we added the condition above
+        switch (errorRes.error.error.message) {
+            case 'EMAIL_EXISTS':
+                errorMessage = 'The email address is already in use by another account.';
+                break;
+            case 'EMAIL_NOT_FOUND':
+                errorMessage = 'There is no user record corresponding to this identifier. The user may have been deleted.';
+                break;
+            case 'INVALID_PASSWORD':
+                errorMessage = 'The password is invalid or the user does not have a password.';
+                break;
+            case 'USER_DISABLED':
+                errorMessage = 'The user account has been disabled by an administrator.';
+                break;
+            case 'TOO_MANY_ATTEMPTS_TRY_LATER : Too many unsuccessful login attempts. Please try again later.':
+                errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+                break;
+            default:
+                break;
+        }
+        return throwError(errorMessage);
     }
 
 }
