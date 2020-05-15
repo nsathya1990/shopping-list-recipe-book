5919672102f34074c2882a35f55beccde3676cd0
Sathyapriyan Chinnathangam <nsathya1990@gmail.com>
2020-05-15 15:09:14 +0530

    improving error handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 interface AuthResponseData {
     idToken: string;
@@ -25,7 +26,27 @@ export class AuthService {
                 email,
                 password,
                 returnSecureToken: true
-            });
+            }).pipe(catchError(errorRes => {
+                let errorMessage = 'An unknown error occurred';
+                if (!errorRes.error || !errorRes.error.error) {
+                    return throwError(errorMessage);
+                }
+                // this scenario will fail for some error such as network failure, therefore we added the condition above
+                switch (errorRes.error.error.message) {
+                    case 'EMAIL_EXISTS':
+                        errorMessage = 'The email address is already in use by another account.';
+                        break;
+                    case 'OPERATION_NOT_ALLOWED':
+                        errorMessage = 'Password sign-in is disabled for this project';
+                        break;
+                    case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+                        errorMessage = 'We have blocked all requests from this device due to unusual activity. Try again later.';
+                        break;
+                    default:
+                        break;
+                }
+                return throwError(errorMessage);
+            }));
     }
 
 }
