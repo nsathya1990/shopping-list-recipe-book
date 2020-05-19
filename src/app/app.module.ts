29f380803fccf69d82ee1076db8bedbbd1c47bd1
Sathyapriyan Chinnathangam <nsathya1990@gmail.com>
2020-05-19 13:56:41 +0530

    more lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,6 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AuthModule } from './auth/auth.module';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 
@@ -21,8 +19,6 @@ import { HeaderComponent } from './header/header.component';
     SharedModule,
     HttpClientModule,
     AppRoutingModule,
-    AuthModule,
-    ShoppingListModule,
     CoreModule
   ],
   providers: [],
