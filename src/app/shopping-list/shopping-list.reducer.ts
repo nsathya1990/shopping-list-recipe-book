dab168a97cc59b25fbf4b9b3ec5c81018397e011
Sathyapriyan Chinnathangam <nsathya1990@gmail.com>
2020-05-23 12:02:01 +0530

    adding logic to the reducer

---
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -0,0 +1,24 @@
+import { Action } from '@ngrx/store';
+
+import { Ingredient } from '../shared/ingredient.model';
+
+const initialState = {
+    ingredients: [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10)
+    ]
+};
+
+// This function will be called by NgRx by passing two arguments
+// 'state' is the current state before it was changed. It is changed by the Reducer
+// 'action' is the action that triggers the Reducer and the state update
+export function ShoppingListReducer(state = initialState, action: Action) {
+    switch (action.type) {
+        // The conevention is to use all uppercase text
+        case 'ADD_INGREDIENT':
+            return {
+                ...state,
+                ingredients: [...state.ingredients, action]
+            };
+    }
+}
